3aa00d22dac31f49204af9d870235a130778da3f
gustavo <gusgurtavo@gmail.com>
2023-12-09 22:13:59 -0300

    add varificação

--- src/app/cadastro-car/cadastro-car.component.ts
+++ src/app/cadastro-car/cadastro-car.component.ts
@@ -1,29 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from './car.model';
 import { ServiceCadastroCarService } from '../service-cadastro-car/service-cadastro-car.service';
-import { FormsModule } from '@angular/forms';
 import { TokenService } from '../storage/storage.service';
 import { Router } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-car',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     NavBarComponent,
     FooterComponent
   ],
   templateUrl: './cadastro-car.component.html',
-  styleUrl: './cadastro-car.component.scss'
+  styleUrls: ['./cadastro-car.component.scss'] // Corrigido para 'styleUrls' (é um array)
 })
-export class CadastroCarComponent {
-  constructor(
-    private carService: ServiceCadastroCarService,
-    private tokenService: TokenService,
-    private router: Router,
-  ) {}
+export class CadastroCarComponent implements OnInit {
   car: Car = {
     nome: '',
     marca: '',
@@ -33,33 +30,45 @@ export class CadastroCarComponent {
     user_id: 0 // substitua pelo ID do usuário, conforme necessário
   };
 
+  constructor(
+    private carService: ServiceCadastroCarService,
+    private tokenService: TokenService,
+    private router: Router,
+  ) {}
+
   ngOnInit(): void {
-    if (this.tokenService.getUserId() === null) {
+    if (!this.tokenService.getUserId()) {
       console.log('Usuário não está logado!');
       this.router.navigate(['/login']);
     }
   }
 
   createCar() {
-    console.log('Criando carro', this.car)
-    console.log(this.tokenService.getUserId());
-    if (this.tokenService.getUserId() === null) {
+    console.log('Criando carro', this.car);
+    if (!this.tokenService.getUserId()) {
       console.log('Usuário não está logado!');
       this.router.navigate(['/login']);
+      return;
     }
 
     this.car.user_id = Number(this.tokenService.getUserId());
     this.carService.createCar(this.car).subscribe(
-      (response: any) => {
-            this.car.user_id = 0;
-            this.car.nome = '';
-            this.car.marca = '';
-            this.car.modelo = '';
-            this.car.price = 0;
-            this.car.url_imagem = '';
-            console.log('Carro cadastrado com sucesso!', response)
+      response => {
+        this.resetCar();
+        console.log('Carro cadastrado com sucesso!', response);
       },
-      (      error: any) => console.error('Erro ao cadastrar carro', error)
+      error => console.error('Erro ao cadastrar carro', error)
     );
+  }
+
+  private resetCar() {
+    this.car = {
+      nome: '',
+      marca: '',
+      modelo: '',
+      price: 0,
+      url_imagem: '',
+      user_id: 0
+    };
   }
 }
